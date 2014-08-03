816148110b3dc10efe59a650872cbbb319c2aaf0
Jed Northridge <jnorthridge@constantcontact.com>
2014-08-03 14:03:29 -0400

    Enabled choice between "fullscreen" images and regular ones.

--- slides.rb
+++ slides.rb
@@ -1,17 +1,32 @@
 require './lib/image_search.rb'
 require 'highline/import'
 
-# Ask for a search term..
-query = ask 'Search For: '
+puts("\nLet's build a slideshow..\n")
 
+#
+# What will we search for?
+query = ask("\nSearch terms: ")
 image_search = ImageSearch.new(query)
+puts
 
+#
+# Should we use regular or fullscreen images?
+image_style = nil
+choose do |menu|
+  menu.prompt = "How should images be displayed?  "
+  menu.choice(:fullscreen) { image_style = 'fullscreen' }
+  menu.choice(:regular) { image_style = 'regular' }
+end
+
+#
+# Here's the presentation DSL..
 presentation do
 
-  slide 'title', query: query
+  # First Slide..
+  slide('title', query: query)
 
+  # Subsequent Slides.. one for each image
   image_search.results.each do |result|
-    slide 'full_screen_image', src: result.src
+    slide(image_style, src: result.src)
   end
-
 end
